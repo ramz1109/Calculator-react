f37c04d82b4a887ef02c5b7389a44a560cd62a49
Ramziddin Suyunov <mrsuyunov1109@gmail.com>
2024-09-06 01:14:41 -0400

    Add buttons and styling for them

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useCallback } from 'react';
+import './index.css';
 
-function App() {
+const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate',
+};
+
+function reducer(state, { type, payload }) {
+
+}
+
+const Button = React.memo(({ dispatch, type, digit, operation, children, className }) => {
+  const handleClick = useCallback(() => {
+    switch (type) {
+      case ACTIONS.ADD_DIGIT:
+        dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } });
+        break;
+      case ACTIONS.CHOOSE_OPERATION:
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } });
+        break;
+      case ACTIONS.CLEAR:
+        dispatch({ type: ACTIONS.CLEAR });
+        break;
+      case ACTIONS.DELETE_DIGIT:
+        dispatch({ type: ACTIONS.DELETE_DIGIT });
+        break;
+      case ACTIONS.EVALUATE:
+        dispatch({ type: ACTIONS.EVALUATE });
+        break;
+      default:
+        break;
+    }
+  }, [dispatch, type, digit, operation]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button className={className} onClick={handleClick}>
+      {children}
+    </button>
+  );
+});
+
+const App = () => {
+  const [{ currentOperand }, dispatch] = useReducer(reducer, {});
+
+  return (
+    <div id="calculator">
+      <input id="display" value={currentOperand ?? ''} readOnly />
+      <div id="keys">
+        <Button dispatch={dispatch} type={ACTIONS.CLEAR} className="operator-key">C</Button>
+        <Button dispatch={dispatch} type={ACTIONS.DELETE_DIGIT}>DEL</Button>
+        <Button dispatch={dispatch} type={ACTIONS.CHOOSE_OPERATION} operation="/">/</Button>
+        <Button dispatch={dispatch} type={ACTIONS.CHOOSE_OPERATION} operation="*">*</Button>
+        <Button dispatch={dispatch} type={ACTIONS.ADD_DIGIT} digit="7">7</Button>
+        <Button dispatch={dispatch} type={ACTIONS.ADD_DIGIT} digit="8">8</Button>
+        <Button dispatch={dispatch} type={ACTIONS.ADD_DIGIT} digit="9">9</Button>
+        <Button dispatch={dispatch} type={ACTIONS.CHOOSE_OPERATION} operation="+">+</Button>
+        <Button dispatch={dispatch} type={ACTIONS.ADD_DIGIT} digit="4">4</Button>
+        <Button dispatch={dispatch} type={ACTIONS.ADD_DIGIT} digit="5">5</Button>
+        <Button dispatch={dispatch} type={ACTIONS.ADD_DIGIT} digit="6">6</Button>
+        <Button dispatch={dispatch} type={ACTIONS.CHOOSE_OPERATION} operation="-">-</Button>
+        <Button dispatch={dispatch} type={ACTIONS.ADD_DIGIT} digit="1">1</Button>
+        <Button dispatch={dispatch} type={ACTIONS.ADD_DIGIT} digit="2">2</Button>
+        <Button dispatch={dispatch} type={ACTIONS.ADD_DIGIT} digit="3">3</Button>
+        <Button dispatch={dispatch} type={ACTIONS.EVALUATE} className="operator-key">=</Button>
+        <Button dispatch={dispatch} type={ACTIONS.ADD_DIGIT} digit="0">0</Button>
+        <Button dispatch={dispatch} type={ACTIONS.ADD_DIGIT} digit=".">.</Button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
